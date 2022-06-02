2559ea878fcb40a9ced0b695d0ec02b6d67e0a60
Anetta <pb20939@student.wsb.bydgoszcz.pl>
2022-06-03 01:21:07 +0200

    03.06.2022 - changes on login site - add API weather (actual weather)

--- src/app/MyComponents/login/login.component.ts
+++ src/app/MyComponents/login/login.component.ts
@@ -14,11 +14,26 @@ export class LoginComponent implements OnInit {
   today_year='';
   today_weekday='';
   users: any;
+  pogoda:any;
+  pogoda_name = '';
+  pogoda_country = '';
+  pogoda_main = '';
+  pogoda_description='';
+  pogoda_icon='';
+  pogoda_temp='';
+  pogoda_feels_like = '';
+  pogoda_pressure='';
+  pogoda_humidity = '';
+
+
+
+
 
   constructor(private http: HttpClient,private router:Router) { }
 
   ngOnInit(): void {
     this.getUsers();
+   this.getWeather();
     this.getToday();
   }
 
@@ -60,7 +75,28 @@ login () {
 }
 
 getUsers() {
-    this.http.get('http://localhost:4200/assets/data/users.json').subscribe((users)=>{this.users = users});
+    this.http.get('http://localhost:4200/assets/data/users.json').subscribe((users)=>{this.users = users}
+);
+
+}
+
+
+getWeather() {
+  this.http.get('https://api.openweathermap.org/data/2.5/weather?q=Bydgoszcz&appid=4f1fc3441b07c9ae898fbc6eb4d08cdc&units=metric&lang=pl').subscribe((pogoda)=>
+  {this.pogoda = pogoda;
+    console.log(this.pogoda.weather[0]["description"])
+
+    this.pogoda_main = this.pogoda.weather[0]["main"];
+    this.pogoda_description=this.pogoda.weather[0]["description"];
+    this.pogoda_icon = `https://openweathermap.org/img/wn/${this.pogoda.weather[0]["icon"]}@2x.png`;
+    this.pogoda_temp=this.pogoda.main.temp;
+    this.pogoda_pressure=this.pogoda.main.pressure;
+    this.pogoda_humidity=this.pogoda.main.humidity;
+    this.pogoda_feels_like=this.pogoda.main.feels_like;
+    this.pogoda_name=this.pogoda.name.toUpperCase();
+    this.pogoda_country=this.pogoda.sys.country;
+  });
+
 }
 
 }
